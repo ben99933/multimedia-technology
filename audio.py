936b180d0c2d19ab92b81fa7e9d755d918405877
ben99933 <yangben999@gmail.com>
2023-10-25 21:06:26 +0800

    updata

--- audio.py
+++ audio.py
@@ -1,5 +1,5 @@
+import cv2
 import moviepy.editor as mpe
-
 
 video = "mandelbrot_animation.mp4"
 audio = "audio.mp3"
@@ -7,4 +7,7 @@ audio = "audio.mp3"
 video_clip = mpe.VideoFileClip(video)
 audio_clip = mpe.AudioFileClip(audio)
 result = video_clip.set_audio(audio_clip)
-result.write_videofile("mandelbrot_animation_with_audio.mp4", fps=24, codec='mpeg4')
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+(width, height) = (1024, 1024)
+fps = 24
+result.write_videofile("mandelbrot_animation_with_audio.mp4", fps=fps, codec="libx264", audio_codec="aac")
